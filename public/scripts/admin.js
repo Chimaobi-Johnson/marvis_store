bc5f2618da6eb014c8eab1d64a72d7d3d92a2fee
Chimaobi Wogu <woguchimaobi@gmail.com>
2021-01-04 23:14:26 +0100

    dashboard components

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,3 +1,27 @@
+// MAP LOGIC
+var ctx = document.getElementById('reportChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        backgroundColor: 'rgba(0, 0, 0, 0.1)'
+    }
+});
+
 
 const showMenu = (menu) => {
     const result = document.getElementById(menu).style.display;
